b177de425f1e3d8efea3ca57bc133c7f7a69ccd0
Douglas Brant <69260468+DouglasBrant@users.noreply.github.com>
2022-05-18 11:49:53 -0300

    Update index.blade.php

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title> Diplomata Hotel</title>
+    <title>Diplomata Hotel</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet">
